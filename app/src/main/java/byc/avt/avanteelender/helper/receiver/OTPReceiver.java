4d5381a6b13058b13f9fce06304bc89b52535dc5
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2020-05-18 07:34:48 +0700

    OTP feature 70
    Need to add permission manager, waiting for SMS API from Avantee

---
+++ app/src/main/java/byc/avt/avanteelender/helper/receiver/OTPReceiver.java
@@ -0,0 +1,30 @@
+package byc.avt.avanteelender.helper.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+
+import com.mukesh.OtpView;
+
+public class OTPReceiver extends BroadcastReceiver {
+
+    private static OtpView otpView;
+
+    public void setEditText(OtpView editText) {
+        OTPReceiver.otpView = editText;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+
+        for (SmsMessage sms : messages) {
+            String message = sms.getMessageBody();
+            String otp = message.split(": ")[1];
+            otpView.setText(otp);
+        }
+    }
+}
