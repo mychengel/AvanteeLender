afc2f78d6d5d7730d2b328872ad3c15ddc083c55
Mychael M. Engel <38174386+mychengel@users.noreply.github.com>
2021-07-08 09:28:52 +0700

    newup

--- app/src/main/java/byc/avt/avanteelender/intro/WalkthroughActivity.java
+++ app/src/main/java/byc/avt/avanteelender/intro/WalkthroughActivity.java
@@ -252,6 +252,7 @@ public class WalkthroughActivity extends AppCompatActivity {
                             Manifest.permission.CAMERA,
                             Manifest.permission.READ_EXTERNAL_STORAGE,
                             Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+                            Manifest.permission.MANAGE_DOCUMENTS,
                             Manifest.permission.READ_PHONE_STATE,
                             Manifest.permission.ACCESS_FINE_LOCATION,
                             Manifest.permission.ACCESS_COARSE_LOCATION
